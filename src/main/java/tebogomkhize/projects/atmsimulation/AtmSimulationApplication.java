d33aca17fed0b04194a66e335b46010651a41e93
Tebogo Mkhize <tebogomkhize01@gmail.com>
2024-09-16 12:35:03 +0200

    feat: ATM Simulation Spring Boot Skeleton

---
+++ src/main/java/tebogomkhize/projects/atmsimulation/AtmSimulationApplication.java
@@ -0,0 +1,13 @@
+package tebogomkhize.projects.atmsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmSimulationApplication.class, args);
+	}
+
+}
